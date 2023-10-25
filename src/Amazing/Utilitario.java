76157c7a108c3a4efcb512df2be4c5eff4f008c1
MatiKK <matiastricker@gmail.com>
2023-10-25 16:06:31 -0300

    calcularPrecioViaje Utilitario

--- src/Amazing/Utilitario.java
+++ src/Amazing/Utilitario.java
@@ -3,6 +3,7 @@ package Amazing;
 public class Utilitario extends Transporte{
 
 	private double valorExtraPorViaje;
+	
 	public Utilitario(String identificador, double volumenMaximoDeCarga, double valorPorViaje, double valorExtraPorViaje) {
 		super(identificador, volumenMaximoDeCarga, valorPorViaje);
 		this.valorExtraPorViaje = valorExtraPorViaje;
