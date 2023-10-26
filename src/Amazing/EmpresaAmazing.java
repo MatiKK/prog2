1285f80af06a8dd375d8329e9a62ea76ec2c7882
MatiKK <matiastricker@gmail.com>
2023-10-26 20:42:25 -0300

    otro error boludo

--- src/Amazing/EmpresaAmazing.java
+++ src/Amazing/EmpresaAmazing.java
@@ -9,46 +9,39 @@ public class EmpresaAmazing implements IEmpresa {
 	private String cuit;
 	private HashMap<String, Transporte> transportes;
 	private HashMap<Integer, Pedido> pedidos;
-
 	private static int NUEVO_IDENTIFICADOR_PEDIDO = 1;
 	private static int NUEVO_IDENTIFICADOR_PAQUETE = 1;
 
 	public EmpresaAmazing(String string) {
 		cuit = string;
-	}// A VER SI FUNCIONAAAAAAAAAAAA
-		// Prueba 2
+	}
 
 	@Override
 	public void registrarAutomovil(String patente, int volMax, int valorViaje, int maxPaq) {
 		if (transportes.containsKey(patente))
 			System.out.println("La patente ingresada ya existe");
-
 		else {
 			Transporte auto = new Automovil(patente, volMax, valorViaje, maxPaq);
 			transportes.put(patente, auto);
 			System.out.println("El automovil fue agregado exitosamente");
 		}
-
 	}
 
 	@Override
 	public void registrarUtilitario(String patente, int volMax, int valorViaje, int valorExtra) {
 		if (transportes.containsKey(patente))
 			System.out.println("La patente ingresada ya existe");
-
 		else {
 			Transporte util = new Utilitario(patente, volMax, valorViaje, valorExtra);
 			transportes.put(patente, util);
 			System.out.println("El transporte utilitario fué agregado exitosamente");
 		}
-
 	}
 
 	@Override
 	public void registrarCamion(String patente, int volMax, int valorViaje, int adicXPaq) {
 		if (transportes.containsKey(patente))
 			System.out.println("La patente ingresada ya existe");
-
 		else {
 			Transporte util = new Camion(patente, volMax, valorViaje, adicXPaq);
 			transportes.put(patente, util);
@@ -57,27 +50,22 @@ public class EmpresaAmazing implements IEmpresa {
 	}
 
 	private int generarNuevoIdentificadorDePedido() {
-		return NUEVO_IDENTIFICADOR_PEDIDO ++;
+		return NUEVO_IDENTIFICADOR_PEDIDO++;
 	}
 
 	private int generarNuevoIdentificadorDePaquete() {
-		return NUEVO_IDENTIFICADOR_PEDIDO ++;
+		return NUEVO_IDENTIFICADOR_PAQUETE++;
 	}
 
 	@Override
 	public int registrarPedido(String cliente, String direccion, int dni) {
-
 		int numPedido = generarNuevoIdentificadorDePedido();
-
 		Pedido pedido = new Pedido(numPedido, dni, cliente, direccion);
-
 		pedidos.put(numPedido, pedido);
 		return numPedido;
-
 	}
 
 	@Override
-	// originalmente iba todo int, despues ver si queda así o no
 	public int agregarPaquete(int codPedido, int volumen, int precio, int costoEnvio) {
 		int identificadorPaquete = generarNuevoIdentificadorDePaquete();
 		Paquete paquete = new PaqueteOrdinario(identificadorPaquete, volumen, precio, costoEnvio);
@@ -97,24 +85,21 @@ public class EmpresaAmazing implements IEmpresa {
 		return p.obtenerIdentificador();
 	}
 
-	private ArrayList<Pedido> listaPedidos(){
+	private ArrayList<Pedido> listaPedidos() {
 		ArrayList<Pedido> listaPedidos = new ArrayList<Pedido>();
-		for (Map.Entry<Integer, Pedido> p: pedidos.entrySet()) {
+		for (Map.Entry<Integer, Pedido> p : pedidos.entrySet()) {
 			listaPedidos.add(p.getValue());
 		}
 		return listaPedidos;
 	}
-	
+
 	@Override
 	public boolean quitarPaquete(int codPaquete) {
-
 		ArrayList<Pedido> pedidos = listaPedidos();
-		
-		for (Pedido pedido: pedidos) {
+		for (Pedido pedido : pedidos) {
 			if (pedido.tienesEstePaquete(codPaquete))
 				return pedido.quitarPaquete(codPaquete);
 		}
-		
 		return false;
 	}
 
